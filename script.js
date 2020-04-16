060a847869ccbe51936e92e56602a0be6dd65890
noah-menikefs <noah.menikefs@ucc.on.ca>
2020-04-16 19:41:07 -0400

    push

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-// Your web app's Firebase configuration
+
+// Initialize Firebase
 var firebaseConfig = {
-	apiKey: "AIzaSyCuB-2Z4UPLutFe68XD1tWT9VnIZ4_xx-4",
-    authDomain: "test-b0203.firebaseapp.com",
-    databaseURL: "https://test-b0203.firebaseio.com",
-    projectId: "test-b0203",
-    storageBucket: "test-b0203.appspot.com",
-    messagingSenderId: "101942352507",
-    appId: "1:101942352507:web:aade72c501a5c023c8a0de",
-    measurementId: "G-Q27358MKXF"
+  apiKey: "AIzaSyCuB-2Z4UPLutFe68XD1tWT9VnIZ4_xx-4",
+  authDomain: "test-b0203.firebaseapp.com",
+  databaseURL: "https://test-b0203.firebaseio.com",
+  projectId: "test-b0203",
+  storageBucket: "test-b0203.appspot.com",
+  messagingSenderId: "101942352507",
+  appId: "1:101942352507:web:aade72c501a5c023c8a0de",
+  measurementId: "G-Q27358MKXF"
 };
-  // Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+var firestore = firebase.firestore();
 
-var db = firebase.firestore();
+var provider = new firebase.auth.GoogleAuthProvider();
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-db.collection("users").add({
-    first: "Noah",
-    last: "Menikefs",
-    born: 2002
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
+const docRef = firestore.doc("samples/sandwichData");
+const outputHeader = document.querySelector("#myName");
+const inputTextField = document.querySelector("#status");
+const saveButton = document.querySelector("#saveButton");
+const loadButton = document.querySelector("#loadButton");
+
+saveButton.addEventListener("click", function(){
+	const textToSave = inputTextField.value;
+	console.log("I am going to save " + textToSave + " to Firestore");
+	docRef.set({
+		hotDogStatus: textToSave
+	}).then(function(){
+		console.log("Status saved!");
+	}).catch(function (error){
+		console.log("Got an error: ", error);
+	});
 });
+
+loadButton.addEventListener("click", function(){
+	docRef.get().then(function (doc){
+		if (doc && doc.exists){
+			const myData = doc.data();
+			outputHeader.innerText = "Hot dog status: " + myData.hotDogStatus;
+		}
+	}).catch(function (error){
+		console.log("Got an error: ", error);
+	});
+});
+
+getRealtimeUpdates = function(){
+	docRef.onSnapshot(function (doc){
+		if (doc && doc.exists){
+			const myData = doc.data();
+			console.log("Check out this document I received", doc);
+			outputHeader.innerText = "Hot dog status: " + myData.hotDogStatus;
+		}
+	});
+}
+
+getRealtimeUpdates();
